83796f81e9c024b16a75627399c748519e913073
PhantomOz <faniogor@gmail.com>
2023-03-23 15:18:32 +0000

    This commit will fix remove function and Add Update quantities function

--- src/app/checkout/cart-items/cart-items.component.ts
+++ src/app/checkout/cart-items/cart-items.component.ts
@@ -9,8 +9,16 @@ export class CartItemsComponent {
   @Input() cart: Cart | undefined;
   @Input() amount: number = 0;
   @Output() removeFromCart: EventEmitter<Cart> = new EventEmitter();
+  @Output() updateCart: EventEmitter<Cart> = new EventEmitter();
 
   handleRemoveCart(product?: Cart): void {
     this.removeFromCart.emit(product);
+  }
+
+  handleAmountChange(quantity: number): void {
+    if (this.cart && quantity) {
+      const Product = { ...this.cart, quantity };
+      this.updateCart.emit(Product);
+    }
   }
 }
--- src/app/checkout/cart/cart.component.ts
+++ src/app/checkout/cart/cart.component.ts
@@ -11,13 +11,24 @@ export class CartComponent implements OnInit {
   total: number = 0;
   constructor(private prdService: ProductsService) {}
   ngOnInit(): void {
+    this.getCart();
+  }
+  getCart(): void {
     this.carts = this.prdService.getFromCart();
     console.log('cart', this.carts);
+    let tots: number = 0;
     this.carts.forEach((cart) => {
-      this.total += cart.quantity * cart.price;
+      tots += cart.quantity * cart.price;
     });
+    this.total = tots;
   }
   removeItem(product: Cart): void {
     this.carts = this.prdService.deleteFromCart(product);
+    this.getCart();
+  }
+
+  updateCartQ(product: Cart): void {
+    this.prdService.updateFromCart(product, product.quantity);
+    this.getCart();
   }
 }
