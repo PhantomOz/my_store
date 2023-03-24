82ba3edf040c85a54aea830b683b98ce6700ae71
PhantomOz <faniogor@gmail.com>
2023-03-24 13:40:36 +0000

    This commit will fix header menu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  ActiveLink: string = 'pl';
 
+  handleClick(e: string): void {
+    this.ActiveLink = e;
+  }
 }
